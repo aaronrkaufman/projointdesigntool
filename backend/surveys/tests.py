3dca34092f6a821601db23811e037ed3eb291843
Akhat <akhatsuleimenov@gmail.com>
2024-03-04 15:16:32 +0400

    feat: Unit tests for restrictions

--- backend/surveys/tests.py
+++ backend/surveys/tests.py
@@ -1,16 +1,14 @@
-import json
+from unittest.mock import patch
+from django.http import HttpResponse
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
+from rest_framework import status
 
 from .models import Survey
 
-
-# Create your tests here.
-
 Profile = get_user_model()
-
 
 class SurveyPostTests(TestCase):
     def setUp(self):
@@ -48,8 +46,6 @@ class SurveyPostTests(TestCase):
                     ],
                 },
             ],
-            "restrictions" : ["att1;==;b;then;att2;==;d"],
-            "cross_restrictions" : ["prof1;att2;!=;d;then;prof2;att3;==;f"],
             "advanced" : {"att1":0, "att2":0, "att3":1}
         }
 
@@ -88,38 +84,7 @@ class SurveyPostTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response.data["message"], "User has no surveys")
-
-    def test_preview_survey_success(self):
-        url = reverse("surveys:preview")
-        response = self.client.post(url, self.payloadSuccess, format="json")
-        self.assertEqual(response.status_code, 201)
-
-    def test_preview_survey_failure(self):
-        url = reverse("surveys:preview")
-        response = self.client.post(url, self.payloadFailure, format="json")
-        self.assertEqual(response.status_code, 400)
-
-    def test_preview_csv_success(self):
-        url = reverse("surveys:preview_csv")
-        response = self.client.post(url, self.payloadSuccess, format="json")
-        self.assertEqual(response.status_code, 201)
-
-    def test_preview_csv_failure(self):
-        payload = {
-            "attributes": [
-                {
-                    "name": "asfasf",
-                    "levels": [],
-                },
-            ]
-        }
-        url = reverse("surveys:preview_csv")
-        response = self.client.post(url, payload, format="json")
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(
-            response.json(),
-            {"Error": "Cannot export to JavaScript. Some attributes have no levels."},
-        )
+        
         
     # def test_qsf_to_attributes_success(self):
     #    qsf_path = "./test.qf"
@@ -134,3 +99,89 @@ class SurveyPostTests(TestCase):
     #     url = reverse("surveys:qualtrics")
     #     response = self.client.post(url, self.payloadSuccess, format="json")
     #     self.assertEqual(response.status_code, 201)
+
+class PreviewSurveyTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.profile = Profile.objects.create_user(
+            username="testprofile",
+            email="testprofile@example.com",
+            password="testpassword123",
+        )
+        self.client.force_authenticate(user=self.profile)
+        self.url = reverse('surveys:preview')  
+
+    def test_preview_survey_success(self):
+        # Вata for a successful request
+        data = {
+            "attributes": [{"name": "attr1", "levels": [{"name": "level1"}, {"name": "level2"}]}],
+            "restrictions": [],
+            "cross_restrictions": [],
+            "profiles": 2
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIn('attributes', response.data)
+        self.assertIn('previews', response.data)
+
+    def test_preview_survey_no_levels(self):
+        # Data where an attribute has no levels
+        data = {
+            "attributes": [{"name": "attr1", "levels": []}],
+            "restrictions": [],
+            "cross_restrictions": [],
+            "profiles": 2
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertDictEqual(response.data, {"Error": "Cannot export to JavaScript. Some attributes have no levels."})
+
+    def test_preview_survey_invalid_data(self):
+        data = {} 
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertDictEqual(response.data, {"Error": "Invalid survey data."})
+        
+class PreviewCSVTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.profile = Profile.objects.create_user(
+            username="testprofile",
+            email="testprofile@example.com",
+            password="testpassword123",
+        )
+        self.client.force_authenticate(user=self.profile)
+        self.url = reverse('surveys:preview_csv')  
+
+    def test_preview_csv_success(self):
+        # Data for a successful request
+        data = {
+            "attributes": [{"name": "attr1", "levels": [{"name": "level1"}, {"name": "level2"}]}],
+            "restrictions": [],
+            "cross_restrictions": [],
+            "profiles": 2
+        }
+        with patch('surveys.views._sendFileResponse') as mock_sendFileResponse:
+            mock_sendFileResponse.return_value = HttpResponse(status=status.HTTP_201_CREATED)
+            response = self.client.post(self.url, data, format='json')
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+            mock_sendFileResponse.assert_called_once()
+
+    def test_preview_csv_no_levels(self):
+        # Data where an attribute has no levels
+        data = {
+            "attributes": [{"name": "attr1", "levels": []}],
+            "restrictions": [],
+            "cross_restrictions": [],
+            "profiles": 2
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertDictEqual(response.data, {"Error": "Cannot export to JavaScript. Some attributes have no levels."})
+
+    def test_preview_csv_invalid_data(self):
+        # Example of sending invalid data
+        data = {} 
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertDictEqual(response.data, {"message": "Invalid survey data."})
