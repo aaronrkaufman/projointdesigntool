19aa1ac059d8269583fec0047e1a497208307a88
Akhat <akhatsuleimenov@gmail.com>
2024-05-27 12:39:31 +0400

    fix: Removed unused admin setup for MD pages

--- backend/dmd_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MarkdownContent
-
-class MarkdownContentAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ["title"]}
-
-admin.site.register(MarkdownContent, MarkdownContentAdmin)
