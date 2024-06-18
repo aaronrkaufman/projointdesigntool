94af5dbc65fd0330c030be46a0c2cbac4446b347
Akhat <akhatsuleimenov@gmail.com>
2024-06-18 12:10:35 +0400

    fix: swagger link

--- backend/projoint/urls.py
+++ backend/projoint/urls.py
@@ -4,13 +4,13 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Swagger urls
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
 
     # path("api/profiles/", include("profiles.urls")),
     path("api/surveys/", include("surveys.urls")),
     path("api/common/", include("common.urls")),
 
+    # Swagger urls
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema")
