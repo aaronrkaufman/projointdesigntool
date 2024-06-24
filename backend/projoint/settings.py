c758ac08c46d3a3505d0718ed78794ed6e19de9b
Akhat <akhatsuleimenov@gmail.com>
2024-06-24 20:23:26 +0400

    fix swagger

--- backend/projoint/settings.py
+++ backend/projoint/settings.py
@@ -138,13 +138,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "profiles.Profile"
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-
-
-SPECTACULAR_SETTINGS = {
-    "swagger": "2.0",
-    'TITLE': 'ProJoint Design Tool API',
-    'DESCRIPTION': 'This is the API documentation for the ProJoint Design Tool. It provides endpoints for managing surveys, profiles, and other common functionalities. Use this documentation to understand and interact with the APIs resources.',
-    'VERSION': '1.0.0',  # API version
-    'OPENAPI_VERSION': '3.0.3',  # OpenAPI spec version
-    'SERVE_INCLUDE_SCHEMA': False,
-}
